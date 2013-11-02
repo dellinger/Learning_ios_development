8d16d6e10450ffdc0a0ba52572ff0f9eed350063
David Ellinger <david.ellinger@me.com>
2013-11-01 20:13:27 -0400

    Hello World Project
    Hour 2 of Sams Teach Yourself iOS6

---
+++ HelloXcode/HelloXcode/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloXcode
+//
+//  Created by David Ellinger on 10/26/13.
+//  Copyright (c) 2013 David Ellinger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
